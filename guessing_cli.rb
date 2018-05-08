49322e195c2dfed6a1d376e6e6e1a31337ea2e16
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 18:20:41 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,12 @@
 # Code your solution here!
+puts "Please enter your guess (number 0 to 100):"
+guess_name = gets.chomp
+computer_guess= rand(0..100)
+if guess_name == computer_guess
+  puts "You guessed the correct number!"
+else
+  puts "The computer guessed #{computer_guess}."
+  puts "enter exit to leave the game"
+  user_respond=gets.chomp
+  if user_respond == "exit"
+    
