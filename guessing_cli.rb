86b9657970fd8c11876653760604cf38d8accef9
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 18:38:01 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,7 @@ loop do
 
 puts "Guess a number between 1 and 6."
 guess_name = gets.chomp
-computer_guess= rand(0..100)
+computer_guess= rand(1..6)
 if guess_name == computer_guess
   puts "You guessed the correct number!"
 elsif guess_name == "exit"
