fad6c4b9f1fe532f231839e572f3a9aef834726b
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 18:34:36 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,18 +1,17 @@
 # Code your solution here!
 def run_guessing_game
+loop do
+
 puts "Please enter your guess (number 0 to 100):"
 guess_name = gets.chomp
 computer_guess= rand(0..100)
 if guess_name == computer_guess
   puts "You guessed the correct number!"
+elsif guess_name == "exit"
+  puts "Bye!"
+  break  
 else
   puts "The computer guessed #{computer_guess}."
-  puts "enter exit to leave the game"
 end
-  user_respond=gets.chomp
-  if user_respond == "exit"
-    break
-  else
-    puts "Please enter again"
-  end
+end
 end
